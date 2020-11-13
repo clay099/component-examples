0712c28029dae27ffabaa0e5bac6023ab5a6c421
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-11-13 15:57:24 -0500

    loader

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Counter from "./Counter";
 import Logo from "./Logo";
+import ContentLoading from "./ContentLoading";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Basic Components</h1>
 			{/* <Logo /> */}
-			<Counter />
+			{/* <Counter /> */}
+			<ContentLoading />
 		</div>
 	);
 }
